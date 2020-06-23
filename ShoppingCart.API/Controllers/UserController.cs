956299b0a15b3e3eefcafadf02390e99dfbaf62f
Buddhika Dhananjaya <dhananjaya_gw@yahoo.com>
2020-06-23 20:19:14 +0530

    Initial Commit

---
+++ ShoppingCart.API/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.API.Models;
+using ShoppingCart.API.Services.Interfaces;
+
+namespace ShoppingCart.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Test")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "API", "Working" };
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ ShoppingCart.API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingCart.API.Entities;
+using ShoppingCart.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Services.Interfaces
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+    }
+}
